33a6f1deb70effbccfce95b7b1b3b8d367c9f4cf
Ayebakuro Ombu <xplicitkuro@gmail.com>
2018-04-10 22:54:51 +0100

    added task 1 php and style
    added task 1 php and style

---
+++ task1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Task1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />    
+</head>
+<body>
+    <div class="frame">
+        <div class="title">
+            HNG Internships
+        </div>
+        <div class="subtitle">
+            Ayebakuro's Task1
+        </div>
+        <div class="intro">
+            The Time Is:
+        </div>
+        <div class="time">
+            <?php echo date("h:i A");?>
+        </div>
+    </div>
+</body>
+</html>
